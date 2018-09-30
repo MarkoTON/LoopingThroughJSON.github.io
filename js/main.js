0bf746e30e6b5aa2c96f74efa1016b09aa18ea92
MarkoTON <MarkoTON@users.noreply.github.com>
2018-09-30 07:41:30 +0200

    New edit version

--- js/main.js
+++ js/main.js
@@ -20,6 +20,8 @@ function onClickPrint(value){
       
       //// Looping kroz objekte osoba
       let i;
+      let m;
+      let sumOfArrays = [];
       for( i = 0;i < peoples.length; i++){
         if(i === value){
           // Stampanje podataka o osobi
@@ -39,40 +41,65 @@ function onClickPrint(value){
           `;
           
           //// Looping kroz prijatelje izabrane osobe
-          let m;
-          for( m = 0; m < peoples[i].friends.length; m++){
-            let friendsID = peoples[i].friends[m];
-            // Stampanje prijatelja izabrane osobe
-            outputFriOfPerson += `<h4 class="green">${peoples[friendsID - 1].firstName} ${peoples[friendsID - 1].surname}</h4>
+          let r;
+          for( r = 0; r < peoples[i].friends.length; r++){
+            let friendsID = peoples[i].friends[r];
+            
+            // Stampanje prijatelja izabrane osobe (Korak 2)
+            outputFriOfPerson += `
+            <h4 class="green">${peoples[friendsID - 1].firstName} ${peoples[friendsID - 1].surname}</h4>
             <!--<p>is ${peoples[friendsID - 1].age} old.</p>
             <p>Gender: <b>${peoples[friendsID - 1].gender}.</b></p>
             <hr>
             <p>${peoples[friendsID - 1].firstName} ${peoples[friendsID - 1].surname} are friends with ${peoples[i].firstName} ${peoples[i].surname} </p>-->
             <hr>
             `;
-            
-            //// Looping kroz prijatelje prijatelja izabrane osobe
-            let j;
-            for( j = 0; j < peoples[m].friends.length; j++){
-              let friendsOfFriendsID = peoples[m].friends[j];
-              // Stampanje prijatelja izabrenih prijatelja
-              outputFriOfFriPerson += `
-              <h4 class="blue">${peoples[friendsOfFriendsID - 1].firstName} ${peoples[friendsOfFriendsID - 1].surname}</h4>
-              <!--<p>is ${peoples[friendsOfFriendsID - 1].age} old.</p>
-              <p>Gender: <b>${peoples[friendsOfFriendsID - 1].gender}.</b></p>
-              <hr>
-              <p>${peoples[friendsOfFriendsID - 1].firstName} ${peoples[friendsOfFriendsID - 1].surname} are friends with ${peoples[m].firstName} ${peoples[m].surname}  </p>-->
-              <hr>
-              `;
-              
-            }
           }
           
+          //// Looping kroz prijatelje izabrane osobe
+          for( m = 0; m < peoples[i].friends.length; m++){
+            sumOfArrays.push(peoples[m].friends);
+          }
+          
+          let newUserArray = Array.prototype.concat(...sumOfArrays);
+          console.log(newUserArray);
+          let reduceUsersArray = newUserArray.reduce(function(a,b){
+            if (a.indexOf(b) < 0 ) a.push(b);
+            return a;
+          },[]);
+          
+          reduceUsersArray.forEach(function(nameUser){
+            // Stampanje prijatelja izabrane osobe
+            outputFriOfFriPerson += `
+            <h4 class="blue">${peoples[nameUser].firstName} ${peoples[nameUser].surname}</h4><hr>
+            `;
+            console.log(peoples[nameUser].firstName);
+          });
+          
+
+          let doubleUsers = newUserArray.slice().sort();
+          let doubleFriendsOfUsers = [];
+          for (let i = 0; i < doubleUsers.length - 1; i++) {
+            if (doubleUsers[i + 1] == doubleUsers[i]) {
+              doubleFriendsOfUsers.push(doubleUsers[i]);
+            }
+          }
+
+          doubleFriendsOfUsers.forEach(function(sugestionUser){
+            // Stampanje predlozenog prijatelja
+            sugestion += `
+            <h4 class="red">${peoples[sugestionUser].firstName} ${peoples[sugestionUser].surname}</h4><hr>
+            `
+            console.log(peoples[sugestionUser].firstName);
+          })
+          
+          // console.log(doubleFriendsOfUsers);
+          
         }
-        document.getElementById('people').innerHTML = outputPerson;
-        document.getElementById('people2').innerHTML = outputFriOfPerson;
+        document.getElementById('people').innerHTML = outputPerson; // r
+        document.getElementById('people2').innerHTML = outputFriOfPerson; // m
         document.getElementById('people3').innerHTML = outputFriOfFriPerson;
-        // document.getElementById('people4').innerHTML = sugestion;
+        document.getElementById('people4').innerHTML = sugestion;
       }
       
     }
