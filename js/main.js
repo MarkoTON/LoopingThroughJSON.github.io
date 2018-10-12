41ddc3e6c0f154fdb722f4197287cf85fc502991
MarkoTON <MarkoTON@users.noreply.github.com>
2018-10-12 19:00:58 +0200

    Finished version

--- js/main.js
+++ js/main.js
@@ -21,15 +21,23 @@ function onClickPrint(value){
       //// Looping kroz objekte osoba
       let i;
       let m;
-      let sumOfArrays = [];
+      let p;
+      let u;
+      
+      let firstPerson;
+      let directFriendIDs = [];//Sadrzi listu prvih prijatelja
+      let fofIDs = [];//Sadrzi listu rijatelja od prijatelja
+      let sugestionIDs = [];
       for( i = 0;i < peoples.length; i++){
         if(i === value){
+          
           // Stampanje podataka o osobi
           outputPerson += `<h1>${peoples[i].firstName} ${peoples[i].surname}</h1> 
           <p>is ${peoples[i].age} old.</p>
           <p>Gender: <b>${peoples[i].gender}.</b></p>
           <hr>
           `;
+          firstPerson = peoples[i];
           
           //// Looping kroz prijatelje izabrane osobe
           let r;
@@ -45,56 +53,84 @@ function onClickPrint(value){
             <p>${peoples[friendsID - 1].firstName} ${peoples[friendsID - 1].surname} are friends with ${peoples[i].firstName} ${peoples[i].surname} </p>-->
             <hr>
             `;
+            directFriendIDs.push(peoples[i].friends[r]);
           }
           
           //// Looping kroz prijatelje izabrane osobe
-          for( m = 0; m < peoples[i].friends.length; m++){
-            sumOfArrays.push(peoples[m].friends);
+          for( m = 0; m < directFriendIDs.length; m++){
+            for (let j = 0; j < peoples.length; j++) {
+              if (directFriendIDs[m] === peoples[j].id) {
+                for (let l = 0; l < peoples[j].friends.length; l++) {
+                  fofIDs.push(peoples[j].friends[l]);
+                }
+              }
+            }
           }
+          // console.log(fofIDs);
           
-          let newUserArray = Array.prototype.concat(...sumOfArrays);
-          console.log(newUserArray);
+          let newUserArray = Array.prototype.concat(...fofIDs);
+          
+          // console.log(newUserArray);
+          sugestionIDs = newUserArray;
+          
           let reduceUsersArray = newUserArray.reduce(function(a,b){
             if (a.indexOf(b) < 0 ) a.push(b);
             return a;
           },[]);
+          // console.log(reduceUsersArray);
+          
+          for(p = 0; p < directFriendIDs.length; p++){
+            for(u = 0; u < reduceUsersArray.length; u++){
+              let index = reduceUsersArray.indexOf(directFriendIDs[p]);
+              let indexMain = reduceUsersArray.indexOf(firstPerson.id);
+              if(index > -1){
+                reduceUsersArray.splice(index, 1);
+              }
+              if(indexMain > -1){
+                reduceUsersArray.splice(indexMain, 1);
+              }
+            }
+          }
+          console.log(reduceUsersArray);
+          
+          for(p = 0; p < directFriendIDs.length; p++){
+            for(u = 0; u < sugestionIDs.length; u++){
+              let index = sugestionIDs.indexOf(directFriendIDs[p]);
+              let indexMain = sugestionIDs.indexOf(firstPerson.id);
+              if(index > -1){
+                sugestionIDs.splice(index, 1);
+              }
+              if(indexMain > -1){
+                sugestionIDs.splice(indexMain, 1);
+              }
+            }
+          }
+          console.log(sugestionIDs);
           
           reduceUsersArray.forEach(function(nameUser){
             // Stampanje prijatelja izabrane osobe
-            outputFriOfFriPerson += `
-            <h4 class="blue">${peoples[nameUser].firstName} ${peoples[nameUser].surname}</h4><hr>
-            `;
-            console.log(peoples[nameUser].firstName);
+            outputFriOfFriPerson += 
+            '<h4 class="blue">' + peoples[nameUser-1].firstName + ' ' + peoples[nameUser-1].surname + '</h4><hr>'
+            ;
+            // console.log(peoples[nameUser-1].firstName);
           });
           
-          
-          //// Predlozeni prijatelji
-          let doubleUsers = newUserArray.slice().sort();
-          let doubleFriendsOfUsers = [];
-          for (let i = 0; i < doubleUsers.length - 1; i++) {
-            if (doubleUsers[i + 1] == doubleUsers[i]) {
-              doubleFriendsOfUsers.push(doubleUsers[i]);
+          let takeSugestionIDs = sugestionIDs.slice().sort();
+          let resultsOfIDs = [];
+          for (let h = 0; h < takeSugestionIDs.length - 1; h++) {
+            if (takeSugestionIDs[h + 1] == takeSugestionIDs[h]) {
+              resultsOfIDs.push(takeSugestionIDs[h]);
             }
           }
-
-          if(doubleFriendsOfUsers.length <= 0){
-            sugestion = `
-            <h4>There is no suggested friends</h4>
-            `;
-          } else {   
-            doubleFriendsOfUsers.forEach(function(sugestionUser){
-              // Stampanje predlozenog prijatelja
-              sugestion += `
-              <h4 class="red">${peoples[sugestionUser].firstName} ${peoples[sugestionUser].surname}</h4><hr>
-              `
-              console.log(peoples[sugestionUser].firstName);
-            })
-          }
-
-
+          console.log(resultsOfIDs);
           
+          //// Predlozeni prijatelji
+          resultsOfIDs.forEach(function(index){
+            // Stampanje predlozenog prijatelja
+            sugestion += '<h4 class="red">' + peoples[index-1].firstName + ' ' + peoples[index-1].surname + '</h4><hr>'
+            ;
+          })
           
-          // console.log(doubleFriendsOfUsers);
           
         }
         document.getElementById('people').innerHTML = outputPerson; // r
